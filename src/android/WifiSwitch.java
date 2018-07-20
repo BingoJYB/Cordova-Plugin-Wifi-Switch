80f47efd70e8c4a737883a28bcaf30ec3e4c0eab
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-07-20 16:30:11 +0200

    Cordova plugin

--- src/android/WifiSwitch.java
+++ src/android/WifiSwitch.java
@@ -12,29 +12,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class WifiSwitch extends CordovaPlugin {
-
     private WifiManager wifiManager;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-
         wifiManager = (WifiManager) cordova.getActivity().getSystemService(Context.WIFI_SERVICE);
     }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
         if (action.equals("switch")) {
-
             wifiManager.setWifiEnabled(data.getBoolean(0));
-
             return true;
-
-        } else {
-            
+        }
+        else {
             return false;
-
         }
     }
 }

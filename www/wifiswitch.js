616c77ac43749ade3b3749ec8256c3b2abe9b601
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-07-26 12:51:09 +0200

    Update wifiswitch.js

--- www/wifiswitch.js
+++ www/wifiswitch.js
@@ -2,6 +2,10 @@ var exec = require('cordova/exec');
 
 module.exports = {
     switch: function (state, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "WifiSwitch", "switch", [state]);
+        cordova.exec(successCallback, errorCallback, "WifiSwitch", "setWifiEnabled", [state]);
+    },
+
+    isWifiEnabled: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "WifiSwitch", "isWifiEnabled", []);
     }
 };
